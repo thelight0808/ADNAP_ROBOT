d87693410b56f4c861891c2358cd827d6770f8db
thelight0808 <dangleminh88@gmail.com>
2015-11-06 17:56:56 +0700

    Create SysConfigure.h

---
+++ SysConfigure/SysConfigure.h
@@ -0,0 +1,19 @@
+/*
+ * SysConfigure.h
+ *
+ *  Created on: Nov 1, 2015
+ *      Author: LENOVO
+ */
+
+#ifndef ADNAP_ROBOT_1_11_SYSCONFIGURE_SYSCONFIGURE_H_
+#define ADNAP_ROBOT_1_11_SYSCONFIGURE_SYSCONFIGURE_H_
+/******************************************************************************
+ * 								PROTOTYPE
+ *
+ *****************************************************************************/
+
+void Captureinit();
+void InitConsole(void);
+extern void SysConfig(void);
+
+#endif /* ADNAP_ROBOT_1_11_SYSCONFIGURE_SYSCONFIGURE_H_ */
